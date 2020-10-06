8042add34cf021b4547f237415cd48326fdac307
Maxi Mino <maximiliano.mino323@gmail.com>
2020-10-06 09:30:04 -0300

    Anoche cerre con un error y no me di cuenta, ya lo arregle

--- formulario.php
+++ formulario.php
@@ -10,4 +10,6 @@ $header = 'Enviado desde el portfolio personal';
 $mensajeCompleto = $comentario . "\nAtentamente: ". $nombre;
 
 mail($destinatario, $asunto, $mensajeCompleto, $header);
+echo "<script> alert('correo enviado')</script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>"
 ?>
